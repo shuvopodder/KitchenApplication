3575cae518d402ff255dc3cf32c490896d803cb5
Shuvo Podder <shuvopodder9@gmail.com>
2023-02-08 00:22:52 +0600

    update

--- app/src/main/java/com/example/kitchenapplication/About_Us.java
+++ app/src/main/java/com/example/kitchenapplication/About_Us.java
@@ -2,6 +2,7 @@ package com.example.kitchenapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class About_Us extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class About_Us extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about__us);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(About_Us.this, MainActivity.class));
     }
 }
